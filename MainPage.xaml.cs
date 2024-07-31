8268dfe8f43e01fa7d3f1f0fc0a6e8730d792da3
gvega001 <gvega001@gmail.com>
2024-07-31 12:26:43 -0700

    Functionality set

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,43 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnBinaryButtonClicked(object sender, EventArgs e)
         {
-            count++;
+            var button = sender as Button;
+            if (button != null)
+            {
+                // Toggle the button's state
+                if (button.Text == "0")
+                {
+                    button.Text = "1";
+                    button.BackgroundColor = Colors.White;
+                    button.TextColor = Colors.Black;
+                }
+                else
+                {
+                    button.Text = "0";
+                    button.BackgroundColor = Colors.Black;
+                    button.TextColor = Colors.White;
+                }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                // Update the decimal value display
+                UpdateDecimalDisplay();
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void UpdateDecimalDisplay()
+        {
+            // Construct the binary string from the button texts
+            string binaryString = $"{Button0.Text}{Button1.Text}{Button2.Text}{Button3.Text}{Button4.Text}{Button5.Text}{Button6.Text}{Button7.Text}";
+            // Convert the binary string to a decimal value
+            int decimalValue = Convert.ToInt32(binaryString, 2);
+            // Update the decimal label
+            DecimalLabel.Text = $"Decimal: {decimalValue}";
         }
     }
-
 }
