acd55804e8ea9fc78a69c3c6a822f8a05db802e3
gvega001 <gvega001@gmail.com>
2024-08-07 19:09:43 -0700

    BitwiseOperations

---
+++ BitwiseOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCalc
+{
+    public static class BitwiseOperations
+    {
+        public static int And(int a, int b) => a & b;
+        public static int Or(int a, int b) => a | b;
+        public static int Xor(int a, int b) => a ^ b;
+        public static int Not(int a) => ~a;
+    }
+
+}
